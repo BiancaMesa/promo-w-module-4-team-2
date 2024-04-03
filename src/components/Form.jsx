e52abc0117256ef5e99b3feec01684adca4db7aa
BiancaMesa <bianca.koraa@gmail.com>
2024-04-03 18:29:58 +0200

    new styles added to ver tarjeta proyecto button

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -13,79 +13,117 @@ function Form({ handleFormAdd, addFormData, setUpdateAvatar, setProjectImg, hand
       <h2 className="title">Información</h2>
       <fieldset className="addForm__group">
         <legend className="addForm__title">Cuéntanos sobre el proyecto</legend>
-        <span className="mandatoryInputs">Los campos marcados con * son obligatorios</span>
-        <input className="addForm__input" type="text"
+        <span className="mandatoryInputs">
+          Los campos marcados con * son obligatorios
+        </span>
+        <input
+          className="addForm__input"
+          type="text"
           name="projectName"
           value={addFormData.projectName}
           id="name"
-          placeholder="Nombre del proyecto *" onChange={handleFormAdd} />
-        <input className="addForm__input" type="text"
+          placeholder="Nombre del proyecto *"
+          onChange={handleFormAdd}
+        />
+        <input
+          className="addForm__input"
+          type="text"
           name="slogan"
           value={addFormData.slogan}
           id="slogan"
           placeholder="Slogan"
-          onChange={handleFormAdd} />
+          onChange={handleFormAdd}
+        />
         <div className="addForm__2col">
-          <input className="addForm__input" type="url"
+          <input
+            className="addForm__input"
+            type="url"
             name="repoLink"
             value={addFormData.repoLink}
             id="repo"
             placeholder="Repositorio *"
-            onChange={handleFormAdd} />
-          <input className="addForm__input" type="url"
+            onChange={handleFormAdd}
+          />
+          <input
+            className="addForm__input"
+            type="url"
             name="demoLink"
             value={addFormData.demoLink}
             id="demo"
             placeholder="Demo"
-            onChange={handleFormAdd} />
+            onChange={handleFormAdd}
+          />
         </div>
-        <input className="addForm__input" type="text"
+        <input
+          className="addForm__input"
+          type="text"
           name="usedTechs"
           value={addFormData.usedTechs}
           id="technologies"
           placeholder="Tecnologías"
-          onChange={handleFormAdd} />
-        <textarea className="addForm__input" type="text"
+          onChange={handleFormAdd}
+        />
+        <textarea
+          className="addForm__input"
+          type="text"
           name="descriptions"
           value={addFormData.descriptions}
           id="desc"
           placeholder="Descripción *"
           rows="5"
-          onChange={handleFormAdd} ></textarea>
+          onChange={handleFormAdd}
+        ></textarea>
       </fieldset>
 
       <fieldset className="addForm__group">
         <legend className="addForm__title">Cuéntanos sobre la autora</legend>
-        <input className="addForm__input"
+        <input
+          className="addForm__input"
           type="text"
           value={addFormData.userName}
-          name="userName" id="autor"
+          name="userName"
+          id="autor"
           placeholder="Nombre *"
-          onChange={handleFormAdd} />
-        <input className="addForm__input" type="text"
+          onChange={handleFormAdd}
+        />
+        <input
+          className="addForm__input"
+          type="text"
           name="userJob"
           value={addFormData.userJob}
           id="job"
           placeholder="Trabajo *"
-          onChange={handleFormAdd} />
+          onChange={handleFormAdd}
+        />
       </fieldset>
       <fieldset className="addForm__group--upload">
         <div className="buttons-upload-container">
-        <GetAvatar setUpdateAvatar={setProjectImg} text="Subir foto del proyecto" />
-        <GetAvatar setUpdateAvatar={setUpdateAvatar} text="Subir foto de la autora" />
+          <GetAvatar
+            setUpdateAvatar={setProjectImg}
+            text="Subir foto del proyecto"
+          />
+          <GetAvatar
+            setUpdateAvatar={setUpdateAvatar}
+            text="Subir foto de la autora"
+          />
         </div>
-       
+
         <div className="buttons-container">
-          <button className="button--large" onClick={handlePost}>Guardar proyecto</button>
+          <button className="button--large" onClick={handlePost}>
+            Guardar proyecto
+          </button>
           {previewUrl && (
             <div className="URLcontainer">
-              <a href={previewUrl} target="_blank">Ver Tarjeta</a>
+              <a href={previewUrl} target="_blank">
+                Ver Tarjeta
+              </a>
             </div>
-          )}    
+          )}
         </div>
+
         <Link to="/cardDetails">
-          <button className="seeProjectCard-btn">Ver Tarjeta Proyecto</button>
-        </Link>
+            <button className="seeProjectCard-btn">Ver Tarjeta Proyecto</button>
+          </Link>
       </fieldset>
     </form>
   );
