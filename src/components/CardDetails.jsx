829d793280f9774c1b47026de0b7617761d73030
BiancaMesa <bianca.koraa@gmail.com>
2024-04-03 16:56:56 +0200

    new classes added to Card Details react and sass components

--- src/components/CardDetails.jsx
+++ src/components/CardDetails.jsx
@@ -1,4 +1,5 @@
-import "../scss/components/Preview.scss";
+//import "../scss/components/Preview.scss";
+import "../scss/components/CardDetails.scss";
 import PropTypes from "prop-types";
 
 function CardDetails({
@@ -6,27 +7,27 @@ function CardDetails({
 
 
   return (
-    <section className="preview">
-      <div className="projectImage" style={{ backgroundImage: `url(${updateProjectImg})` }}>  </div>
-      <article className="card">
-        <h2 className="card__projectTitle">
-          <span className="card__projectTitle--text">
+    <section className="cardDetailsContainer">
+      <div className="projectImg" style={{ backgroundImage: `url(${updateProjectImg})` }}>  </div>
+      <article className="cardDetails">
+        <h2 className="cardDetails__projectTitle">
+          <span className="cardDetails__projectTitle--text">
             Personal Project Card
           </span>
         </h2>
 
-        <div className="card__author">
-          <div className="card__authorPhoto" style={{ backgroundImage: `url(${updateAvatar})` }}>  </div>
-          <p className="card__job">{addFormData.userJob || "Professional Baroque Artist"}</p>
-          <h3 className="card__name">{addFormData.userName || "Diego Velázquez"}</h3>
+        <div className="cardDetails__author">
+          <div className="cardDetails__authorPhoto" style={{ backgroundImage: `url(${updateAvatar})` }}>  </div>
+          <p className="cardDetails__job">{addFormData.userJob || "Professional Baroque Artist"}</p>
+          <h3 className="cardDetails__name">{addFormData.userName || "Diego Velázquez"}</h3>
         </div>
 
-        <div className="card__project">
+        <div className="cardDetails__project">
 
-          <h3 className="card__name">{addFormData.projectName || "Personal Project Card"}</h3>
-          <p className="card__slogan">{addFormData.slogan || "“Velázquez: Donde la grandeza de la corte cobra vida en el lienzo.”"}</p>
-          <h3 className="card__descriptionTitle">Product description</h3>
-          <p className="card__description">{addFormData.descriptions || `Designado por la corte de Carlos III, me enfrento al reto de plasmar “Las Meninas”, un proyecto donde arte y visión de la corte se entrelazan. Como Diego Velázquez, utilizo técnicas de claroscuro y perspectiva para capturar la complejidad y el esplendor del entorno, con cada figura meticulosamente posicionada reflejando las expectativas de mis mecenas, los verdaderos Product Owners. Este lienzo es mi campo de trabajo, donde cada pincelada une técnica y narrativa en una obra maestra destinada a perdurar.`}</p>
+          <h3 className="cardDetails__name">{addFormData.projectName || "Personal Project Card"}</h3>
+          <p className="cardDetails__slogan">{addFormData.slogan || "“Velázquez: Donde la grandeza de la corte cobra vida en el lienzo.”"}</p>
+          <h3 className="cardDetails__descriptionTitle">Product description</h3>
+          <p className="cardDetails__description">{addFormData.descriptions || `Designado por la corte de Carlos III, me enfrento al reto de plasmar “Las Meninas”, un proyecto donde arte y visión de la corte se entrelazan. Como Diego Velázquez, utilizo técnicas de claroscuro y perspectiva para capturar la complejidad y el esplendor del entorno, con cada figura meticulosamente posicionada reflejando las expectativas de mis mecenas, los verdaderos Product Owners. Este lienzo es mi campo de trabajo, donde cada pincelada une técnica y narrativa en una obra maestra destinada a perdurar.`}</p>
 
           <a
 
@@ -35,11 +36,11 @@ function CardDetails({
 
           >{previewUrl}</a>
 
-          <div className="card__technicalInfo">
-            <p className="card__technologies">{addFormData.usedTechs || "Oil painting"}</p>
+          <div className="cardDetails__technicalInfo">
+            <p className="cardDetails__technologies">{addFormData.usedTechs || "Oil painting"}</p>
 
             <a
-              className="icon icon__www"
+              className="iconDetails iconDetails__www"
               href={addFormData.demoLink}
               target="_blank"
               title="Haz click para ver el proyecto online"
@@ -47,14 +48,14 @@ function CardDetails({
               Web link
             </a>
             <a
-              className="icon icon__github"
+              className="iconDetails iconDetails__github"
               href={addFormData.repoLink}
               target="_blank"
               title="Haz click para ver el código del proyecto"
             >
               GitHub link
             </a>
-            <button type="button" className="button-reset" onClick={handleReset}>Reiniciar</button>
+            <button type="button" className="buttonDetails-reset" onClick={handleReset}>Reiniciar</button>
           </div>
         </div>
       </article>
