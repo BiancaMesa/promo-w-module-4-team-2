48402ab8486018108655c1f384e43550899e17fb
BiancaMesa <bianca.koraa@gmail.com>
2024-04-05 13:15:06 +0200

    author and project data relationship created

--- BD/Database.sql
+++ BD/Database.sql
@@ -15,3 +15,26 @@ jobName VARCHAR(50) NOT NULL,
 authorImage VARCHAR(3000) NOT NULL
 );
 
+
+ALTER TABLE author ADD COLUMN idAuthor INT auto_increment PRIMARY KEY;
+
+SELECT * FROM projectData;
+
+SELECT author.authorName, author.jobName, projectData.projectName 
+FROM projectData, author
+WHERE projectData.fk_idAuthor = author.idAuthor
+ORDER BY projectData.projectName ASC;
+
+UPDATE author SET jobName = 'millionaire$$$' WHERE idAuthor = 3;
+
+ALTER TABLE projectData MODIFY projectImage text not null;
+ALTER TABLE author MODIFY authorImage text not null;
+
+-- Para modificar la estructura base de una tabla:
+-- ALTER TABLE añadir columna nueva (ADD COLUMN) o modificar una antigua (MODIFY) o sólo cambiar tipo dato (CHANGE)
+
+-- Para añadir/modificar los valores de una tabla:
+-- INSERT INTO añadir valores a columnas por primera vez
+-- UPDATE modificar el valor de una columna concreta cuando el resto ya están escritas
+
+
