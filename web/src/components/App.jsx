e85f9dec6aa497323ef584ef0ae0ce137f25c3e5
Alba Melchor Gomez <albamelchorgomez@gmail.com>
2024-04-17 12:52:28 +0200

    to try with enviroment variable

--- web/src/components/App.jsx
+++ web/src/components/App.jsx
@@ -61,7 +61,7 @@ function App() {
 
   useEffect(() => {
     async function getProjects() {
-      const response = await fetch(`http://localhost:4000/projects`);
+      const response = await fetch(`https://proyectos-paintones.onrender.com/projects`);
       const data = await response.json();
       setProjectsList(data.message);
     }
@@ -74,7 +74,7 @@ function App() {
     event.preventDefault();
     console.log("clicko");
 
-    fetch(`http://localhost:4000/projects`, {
+    fetch(`https://proyectos-paintones.onrender.com/projects`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
