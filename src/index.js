ca5920e9aa2a2725b839bed714c45795129d45a9
BiancaMesa <bianca.koraa@gmail.com>
2024-04-18 12:30:06 +0200

    app changed for server

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ require("dotenv").config();
 
 const server = express();
 const path = require('path');
-app.use(express.static(path.join(__dirname, 'build')));
+server.use(express.static(path.join(__dirname, 'build')));
 
-app.get('*', function(req, res) {
+server.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
